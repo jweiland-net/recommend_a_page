b895ec20bbe8841e25bc576d0d707b4e856b501e
Shiryo (Markus Kugler) <info@ma-ku.eu>
2016-09-06 16:19:01 +0200

    Fixed missing pid in db

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,11 +3,14 @@
 #
 CREATE TABLE tx_recommendapage_domain_model_recommendedpage (
 	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
 
 	referrer_pid int(11) unsigned DEFAULT '0' NOT NULL,
 	target_pid int(11) unsigned DEFAULT '0' NOT NULL,
 
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid)
+	PRIMARY KEY (uid),
+	KEY parent (pid)
 );
