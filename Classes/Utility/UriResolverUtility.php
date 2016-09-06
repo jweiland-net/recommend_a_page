df6a57ddc85641f8e6113d5be77a7e2097f47506
Shiryo (Markus Kugler) <info@ma-ku.eu>
2016-09-06 15:53:28 +0200

    Removed Debugger

--- Classes/Utility/UriResolverUtility.php
+++ Classes/Utility/UriResolverUtility.php
@@ -113,7 +113,6 @@ class UriResolverUtility
                 $this->getSpeakingUrl($uri),
                 (int)GeneralUtility::_GET('L')
             );
-            DebuggerUtility::var_dump($convertedUrl);
             $pid = $convertedUrl->getPageId();
         } else {
             $pid = $this->getGetParams($uri)['id'];
