b72fd0e23bdd999813bccf1e4b310bcb7e85efa3
Shiryo (Markus Kugler) <info@ma-ku.eu>
2016-09-12 15:36:55 +0200

    [Bugfix] Fixed a bug where only one recommended page was created

--- Classes/Task/LoadRecommendedPagesTask.php
+++ Classes/Task/LoadRecommendedPagesTask.php
@@ -110,10 +110,10 @@ class LoadRecommendedPagesTask extends AbstractTask
                 $recommendedPages = $this->piwikDatabaseService->getTargetPids($idaction);
                 DebuggerUtility::var_dump($recommendedPages);
                 
+                $updateList[$idaction] = array();
                 foreach ($recommendedPages as $targetPage) {
                     $targetPid = $mappedPages[$targetPage['targetPid']];
                     if ($targetPid != null) {
-                        $updateList[$idaction] = array();
                         array_push($updateList[$idaction], array(
                             'referrer_pid' =>  $typo3Pid,
                             'target_pid' => $targetPid
