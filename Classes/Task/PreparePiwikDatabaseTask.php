824ac4fae02363ad1127e7ff2c4810eaec2291ed
Shiryo (Markus Kugler) <info@ma-ku.eu>
2016-09-12 12:20:14 +0200

    test

--- Classes/Task/PreparePiwikDatabaseTask.php
+++ Classes/Task/PreparePiwikDatabaseTask.php
@@ -48,6 +48,7 @@ class PreparePiwikDatabaseTask extends AbstractTask
         
         foreach ($piwikActions as $action)
         {
+            DebuggerUtility::var_dump($action);
             DebuggerUtility::var_dump($uriResolverUtility->getTYPO3PidFromUri($action['name']));
             $piwikDatabaseService->updateRows(
                 array(
