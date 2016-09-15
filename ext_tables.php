3ba7c76378a2de1fdad2004192107a5810ffd6e4
Shiryo (Markus Kugler) <info@ma-ku.eu>
2016-09-15 10:14:50 +0200

    Resolves #5 - ext_localconf.php now uses language files

--- ext_tables.php
+++ ext_tables.php
@@ -8,6 +8,8 @@ if (TYPO3_MODE === 'BE') {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
         'JWeiland.' . $_EXTKEY,
         'recommendPages',
-        'Recommend Pages'
+        'LLL:EXT:' .
+        $_EXTKEY .
+        '/Resources/Private/Language/locallang.xlf:plugin.recommendapage.title'
     );
 }
