80ce2b1a90c26cbc4427c275d4d7d0cec5d5e5d7
Shiryo (Markus Kugler) <info@ma-ku.eu>
2016-09-16 10:40:59 +0200

    Resolves #23 - Now splitting insert querys at 300 inserts per query

--- Classes/Task/LoadRecommendedPagesTask.php
+++ Classes/Task/LoadRecommendedPagesTask.php
@@ -130,14 +130,19 @@ class LoadRecommendedPagesTask extends AbstractTask
     {
         $this->getDatabaseConnection()->exec_TRUNCATEquery('tx_recommendapage_domain_model_recommendedpage');
         
+        $offset = 0;
+        $nextOffset = 300;
+        
         foreach ($pages as $recommendedPages) {
+            $pagesToInsert = array_splice($recommendedPages, $offset, $nextOffset);
             $this->getDatabaseConnection()->exec_INSERTmultipleRows(
                 'tx_recommendapage_domain_model_recommendedpage',
                 array(
                     'referrer_pid', 'target_pid'
                 ),
-                $recommendedPages
+                $pagesToInsert
             );
+            $offset += $nextOffset;
         }
     }
     
